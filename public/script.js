5f85490c1af553179e1586769983d31097994efc
Aditya <tvaditya@gmail.com>
2020-06-21 18:26:28 -0300

    Learning webpack

---
+++ public/script.js
@@ -0,0 +1,3 @@
+// Babel, converte codigo (transpiles) do React para um que o browser entenda.
+// Webpack, para cada tipo de arquivos (.js, .css, .png) converte o codigo de uma maneira diferente
+//
